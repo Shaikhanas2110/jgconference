3fa1e5ab5f4d03a9d80a8f221ea301c6a2146dd8
Shaikhanas2110 <145525463+Shaikhanas2110@users.noreply.github.com>
2026-08-13 21:58:09 +0530

    Image path added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ import {
 import { MdLocationOn } from "react-icons/md";
 import { NavLink, useLocation } from "react-router-dom";
 import image from "../assets/image.jpeg";
-import about from "public/about.jpg";
+import about from "../assets/IMG_2306.jpeg";
 
 function Home() {
   const location = useLocation();
